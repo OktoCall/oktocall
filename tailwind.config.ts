b2585bf2d55efcb3447a9c7b053bcd56a7ee5df0
matias062169 <matiasezequiel062169@gmali.com>
2023-12-11 20:29:55 -0300

    boceto de la pagina de bienvenida, medidas: 432x702

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,14 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'bienvenida': "url('/Fondo-de-bienvenida-cel.jpg')"
       },
+      margin: {
+        '28': '7rem',
+      },
+      padding:{
+        '18': '4.5rem'
+      }
     },
   },
   plugins: [],
